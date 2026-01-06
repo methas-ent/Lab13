5c398c5a5de281c57c542299116ae46b4646f4d6
Methas <methas_n@cmu.ac.th>
2026-01-06 21:32:33 +0700

    "lab13_1.cpp completed"

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,43 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    
+    double sum = 0 , mean =0;
+    for(int i = 0 ; i < N ; i++){
+        sum+=A[i];
+    }
+    mean = sum/N ;
+    B[0] = mean;
+
+    double sixma = 0;
+    for(int j = 0 ; j<N ; j++){
+        sixma += pow((A[j] - B[0]),2.0); 
+    }
+    B[1] = pow(((1.0/N)*sixma),0.5);
+    
+    double GM = 1.0;
+    for(int k = 0 ; k<N ; k++){
+        GM *= A[k];
+    }
+    B[2] = pow(GM,1.0/N);
+
+    double HM = 0;
+    for(int l = 0 ; l<N ; l++){
+        HM += 1.0/A[l]; 
+    }
+    B[3] = N/HM;
+
+    double Max = A[0];
+    for(int m = 0 ; m<N ; m++){
+        if(A[m] > Max) Max = A[m];
+    }
+    B[4] = Max;
+
+    double Min = A[0];
+    for(int n = 0 ; n<N ; n++){
+        if(A[n] < Min) Min = A[n];
+    }
+    B[5] = Min;
+}
